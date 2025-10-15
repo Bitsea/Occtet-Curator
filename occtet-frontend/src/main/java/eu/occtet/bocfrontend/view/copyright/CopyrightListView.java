b081830059516bd9ffb163c678dee95bf025b610
Alessandro.Castielli <Alessandro.Castielli@bitsea.de>
2025-10-15 17:00:28 +0200

    Updated vulnerability detail view and added feature "Findings". Updated all list views with genericFilters and fixed small bugs

--- occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/copyright/CopyrightListView.java
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/copyright/CopyrightListView.java
@@ -22,36 +22,25 @@
 
 package eu.occtet.bocfrontend.view.copyright;
 
-import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.accordion.Accordion;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
 import eu.occtet.bocfrontend.dao.CopyrightRepository;
-import eu.occtet.bocfrontend.dao.ProjectRepository;
-import eu.occtet.bocfrontend.dao.SoftwareComponentRepository;
 import eu.occtet.bocfrontend.entity.Copyright;
-import eu.occtet.bocfrontend.entity.Project;
-import eu.occtet.bocfrontend.entity.SoftwareComponent;
 import eu.occtet.bocfrontend.service.CopyrightService;
-import eu.occtet.bocfrontend.service.SoftwareComponentService;
 import eu.occtet.bocfrontend.view.main.MainView;
 import io.jmix.flowui.Notifications;
-import io.jmix.flowui.component.combobox.JmixComboBox;
 import io.jmix.flowui.component.upload.FileUploadField;
 import io.jmix.flowui.download.Downloader;
 import io.jmix.flowui.kit.component.upload.event.FileUploadSucceededEvent;
 import io.jmix.flowui.model.CollectionContainer;
-import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -65,43 +54,10 @@ public class CopyrightListView extends StandardListView<Copyright> {
     private static final Logger log = LogManager.getLogger(CopyrightListView.class);
 
     @ViewComponent
-    private JmixComboBox<Project> projectComboBox;
-
-    @ViewComponent
-    private JmixComboBox<SoftwareComponent> softwareComponentComboBox;
-
-    @ViewComponent
-    private JmixComboBox<String> curatedComboBox;
-
-    @ViewComponent
-    private JmixComboBox<String> garbageComboBox;
-
-    @ViewComponent
     private CollectionContainer<Copyright> copyrightsDc;
-
-    @ViewComponent
-    private CollectionLoader<Copyright> copyrightsDl;
-
-    @ViewComponent
-    private TextField searchField;
-
-    @ViewComponent
-    private Accordion projectAccordion;
-
-    @ViewComponent
-    private Accordion attributesAccordion;
-
-    @ViewComponent
-    private FileUploadField uploadFile;
-
-    @Autowired
-    private ProjectRepository projectRepository;
 
     @Autowired
     private CopyrightService copyrightService;
-
-    @Autowired
-    private SoftwareComponentService softwareComponentService;
 
     @Autowired
     private Downloader downloader;
@@ -110,81 +66,14 @@ public class CopyrightListView extends StandardListView<Copyright> {
     private Notifications notifications;
 
     @Autowired
-    private SoftwareComponentRepository softwareComponentRepository;
-
-    @Autowired
     private CopyrightRepository copyrightRepository;
 
     private List<Copyright> copyrightsList;
-    private SoftwareComponent softwareComponent;
-
-    private final List<String> valuesOfBoolean = List.of("True","False");
-
-    private static final String CURATED = "curated";
-    private static final String GARGABE = "garbage";
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event){
 
-        projectComboBox.setItems(projectRepository.findAll());
-        projectComboBox.setItemLabelGenerator(Project::getProjectName);
-
-        softwareComponentComboBox.setItems(softwareComponentRepository.findAll());
-        softwareComponentComboBox.setItemLabelGenerator(softwareComponent ->
-                softwareComponent.getName()+" "+softwareComponent.getVersion());
-
-        curatedComboBox.setItems(valuesOfBoolean);
-        garbageComboBox.setItems(valuesOfBoolean);
-
-        projectAccordion.close();
-        attributesAccordion.close();
-
         copyrightsList = copyrightRepository.findAll();
-    }
-
-    @Subscribe("projectComboBox")
-    public void showComponentsFromProject(final AbstractField.ComponentValueChangeEvent<JmixComboBox<Project>, Project> event){
-        applyAllFilters();
-    }
-
-    @Subscribe("softwareComponentComboBox")
-    public void showCopyrightsFromSoftwareComponentComboBox(final AbstractField.ComponentValueChangeEvent<JmixComboBox<SoftwareComponent>,
-            SoftwareComponent> event){
-        applyAllFilters();
-    }
-
-    @Subscribe("curatedComboBox")
-    public void findCopyrightsByCurated(final AbstractField.ComponentValueChangeEvent<JmixComboBox<String>,String> event){
-        applyAllFilters();
-    }
-
-    @Subscribe("garbageComboBox")
-    public void findCopyrightsByGarbage(final AbstractField.ComponentValueChangeEvent<JmixComboBox<String>,String> event){
-        applyAllFilters();
-    }
-
-    @Subscribe("searchButton")
-    public void searchCopyright(ClickEvent<Button> event) {
-
-        String searchWord = searchField.getValue();
-        if (!searchWord.isEmpty() && event != null) {
-
-            List<Copyright> allCopyrights = copyrightsDc.getItems();
-            List<Copyright> searchCopyrights = new ArrayList<>();
-
-            for(Copyright copyright : allCopyrights){
-
-                if(copyright.getCopyrightText() != null){
-                    if(copyright.getCopyrightText().toUpperCase().contains(searchWord.toUpperCase()))
-                        searchCopyrights.add(copyright);
-                }
-            }
-            if(!searchCopyrights.isEmpty()){
-                copyrightsDc.setItems(searchCopyrights);
-            }
-        }else if(searchWord.isEmpty() && event != null){
-            copyrightsDl.load();
-        }
     }
 
     @Subscribe("downloadButton")
@@ -217,30 +106,6 @@ public class CopyrightListView extends StandardListView<Copyright> {
                 getInfoMessage("Upload failed!");
             }
         }
-    }
-
-    private void applyAllFilters(){
-        Project project = projectComboBox.getValue();
-        SoftwareComponent softwareComponent = softwareComponentComboBox.getValue();
-        Boolean curated = curatedComboBox.getValue() != null ? Boolean.parseBoolean(curatedComboBox.getValue()) : null;
-        Boolean garbage = garbageComboBox.getValue() != null ? Boolean.parseBoolean(garbageComboBox.getValue()) : null;
-
-        List<Copyright> filteredCopyrights = new ArrayList<>(copyrightsList);
-
-        if (project != null){
-            filteredCopyrights.retainAll(copyrightService.findCopyrightsByProject(project));
-        }
-        if (softwareComponent != null){
-            filteredCopyrights.retainAll(copyrightService.findCopyrightsBySoftwareComponent(softwareComponent));
-        }
-        if (curated != null){
-            filteredCopyrights.retainAll(copyrightService.findCopyrightsByCurated(curated));
-        }
-        if (garbage != null){
-            filteredCopyrights.retainAll(copyrightService.findCopyrightsByGarbage(garbage));
-        }
-
-        copyrightsDc.setItems(filteredCopyrights);
     }
 
     private void getInfoMessage(String message){
--- occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/project/ProjectListView.java
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/project/ProjectListView.java
@@ -22,18 +22,10 @@
 
 package eu.occtet.bocfrontend.view.project;
 
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import eu.occtet.bocfrontend.entity.Project;
 import eu.occtet.bocfrontend.view.main.MainView;
-import io.jmix.flowui.model.CollectionContainer;
-import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Route(value = "projects", layout = MainView.class)
@@ -43,33 +35,4 @@ import java.util.List;
 @DialogMode(width = "64em")
 public class ProjectListView extends StandardListView<Project> {
 
-
-    @ViewComponent
-    private TextField searchField;
-
-    @ViewComponent
-    private CollectionContainer<Project> projectsDc;
-
-    @ViewComponent
-    private CollectionLoader<Project> projectsDl;
-
-
-    @Subscribe("searchButton")
-    public void searchProjects(ClickEvent<Button> event){
-
-        String searchWord = searchField.getValue();
-        if(!searchWord.isEmpty() && event != null){
-
-            List<Project> projects = projectsDc.getItems();
-            List<Project> searchProjects = new ArrayList<>();
-
-            for(Project project : projects){
-                if(project.getProjectName().toUpperCase().contains(searchWord.toUpperCase()))
-                    searchProjects.add(project);
-            }
-            projectsDc.setItems(searchProjects);
-        }else if(searchWord.isEmpty() && event != null){
-            projectsDl.load();
-        }
-    }
 }
