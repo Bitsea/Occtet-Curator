fb2a23ca532d49da7a0039546164587ffceb8342
Lelalue <leoni.tischer@bitsea.de>
2025-11-13 11:03:09 +0100

    adding license text search

---
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/dao/LicenseDao.java
@@ -0,0 +1,36 @@
+package eu.occtet.bocfrontend.dao;
+
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class LicenseDao {
+
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    public LicenseDao(@Autowired DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<Pair<UUID,Float>> findLicenseIdsSimilarTo(String text, int limit) {
+
+        String sql = "select *, similarity(license_text,?) as rank " +
+                "from LICENSE order by rank desc limit ?;";
+
+        List<Map<String, Object>> result= jdbcTemplate.queryForList(sql, text, limit);
+        return result.stream()
+                .filter(m-> (Float)m.get("rank")!=null && (Float)m.get("rank")>0)
+                .map(map-> Pair.of((UUID)map.get("id"),(Float)map.get("rank")))
+                .collect(Collectors.toList());
+    }
+
+}
--- occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/license/LicenseListView.java
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/license/LicenseListView.java
@@ -25,15 +25,36 @@ package eu.occtet.bocfrontend.view.license;
 
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.data.renderer.TextRenderer;
 import com.vaadin.flow.router.Route;
+import eu.occtet.bocfrontend.dao.LicenseRepository;
 import eu.occtet.bocfrontend.entity.License;
 import eu.occtet.bocfrontend.service.SPDXLicenseService;
 import eu.occtet.bocfrontend.view.main.MainView;
+import eu.occtet.bocfrontend.view.services.LicenseTextService;
+import io.jmix.core.Messages;
+import io.jmix.flowui.DialogWindows;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.component.grid.DataGridColumn;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.model.CollectionContainer;
 import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.beans.PropertyChangeEvent;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 
 @Route(value = "licenses", layout = MainView.class)
@@ -45,16 +66,87 @@ public class LicenseListView extends StandardListView<License> {
 
     private static final Logger log = LogManager.getLogger(LicenseListView.class);
 
+    private final static String SIMILARITY_RANK_COLUMN_ID ="Similarity Rank";
+    private static final int MAX_FULLTEXT_RESULTS = 50;
+
     @ViewComponent
     private CollectionLoader<License> licensesDl;
 
     @Autowired
     private SPDXLicenseService spdxLicenseService;
 
+    @Autowired
+    private DialogWindows dialogWindows;
+
+    @Autowired
+    private LicenseTextService licenseTextService;
+    @ViewComponent
+    private DataGrid<License> licensesDataGrid;
+
+    @Autowired
+    private Messages messages;
+
+    @ViewComponent
+    private CollectionContainer<License> licensesDc;
+
+    @Autowired
+    private LicenseRepository licenseRepository;
+
+    @Subscribe(id = "textSearchLicense", subject = "singleClickListener")
+    public void onTextSearchLicenseClick(final ClickEvent<JmixButton> event) {
+        checkAndRemoveSimilarityColumn();
+        log.debug("clicked textsearchbutton");
+        DialogWindow<LicenseDialogWindowView> window =
+                dialogWindows.view(this, LicenseDialogWindowView.class).build();
+        window.setResizable(true);
+        window.setWidth("70%");
+        window.setHeight("70%");
+        window.addAfterCloseListener(afterCloseEvent -> {
+            if (afterCloseEvent.closedWith(StandardOutcome.SAVE)) {
+                doFulltextSearch(window.getView().getText());
+            }
+        });
+        window.open();
+    }
+
+    private void doFulltextSearch(String search) {
+        if (!StringUtils.isEmpty(search)) {
+            List<Pair<License, Float>> licenseTypes = new ArrayList<>(licenseTextService.findBySimilarity(search, MAX_FULLTEXT_RESULTS));
+            DataGridColumn dgc =licensesDataGrid.addColumn(new TextRenderer<>(entity -> {
+                AtomicReference<String> rankLabel = new AtomicReference<>("");
+                Optional<Float> rankValue= licenseTypes.stream()
+                        .filter(lt -> entity.getLicenseType().equals(lt.getKey().getLicenseType()))
+                        .map(Pair::getValue).findFirst();
+                rankValue.ifPresent(rank -> rankLabel.set("" + new DecimalFormat("#").format(rank * 100)));
+                return rankLabel.get();
+            }));
+            String key=messages.getMessage(getClass(), "similarityRank");
+            if (licensesDataGrid.getColumnByKey(key)==null) {
+                dgc.setKey(messages.getMessage(getClass(), "similarityRank"));
+                dgc.setHeader(messages.getMessage(getClass(), "similarityRank"));
+            }
+            licensesDc.setItems(licenseTypes.stream().map(Pair::getKey).collect(Collectors.toList()));
+        } else {
+            licensesDc.setItems( licenseRepository.findAll());
+        }
+    }
+
+    private void checkAndRemoveSimilarityColumn() {
+        String key=messages.getMessage(getClass(), "similarityRank");
+        if (licensesDataGrid.getColumnByKey(key)!=null)
+            licensesDataGrid.removeColumnByKey(key);
+    }
 
     @Subscribe("fetchSPDXButton")
     public void fetchSPDX_Licenses(ClickEvent<Button> event){
+        checkAndRemoveSimilarityColumn();
         spdxLicenseService.readDefaultLicenseInfos();
         licensesDl.load();
     }
+
+
+
+
+
+
 }
