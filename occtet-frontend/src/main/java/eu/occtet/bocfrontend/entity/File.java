10a54fd83b6a1d6ca79bd3985966f3b831706d8f
Ibrahim.AlBahra <ibrahim.albahra@bitsea.de>
2025-11-27 16:56:57 +0100

    add File entity to frontend, #45

---
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/entity/File.java
@@ -0,0 +1,226 @@
+/*
+ *  Copyright (C) 2025 Bitsea GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https:www.apache.orglicensesLICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  License-Filename: LICENSE
+ *
+ *
+ */
+
+package eu.occtet.bocfrontend.entity;
+
+import io.jmix.core.DeletePolicy;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.entity.annotation.OnDelete;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "FILE", indexes = {
+        @Index(name = "IDX_FILE_PROJECT_PARENT", columnList = "PROJECT_ID, PARENT_ID"),
+        @Index(name = "IDX_FILE_PARENT", columnList = "PARENT_ID")
+})
+@Entity
+public class File {
+
+    @JmixGeneratedValue
+    @Id
+    @Column(name = "ID", nullable = false)
+    private UUID id;
+
+    @InstanceName
+    @Column(name = "FILENAME", nullable = false)
+    private String fileName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PROJECT_ID", nullable = false)
+    @OnDelete(DeletePolicy.CASCADE)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PARENT_ID")
+    @OnDelete(DeletePolicy.CASCADE)
+    private File parent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CODELOCATION_ID", nullable = true)
+    @OnDelete(DeletePolicy.UNLINK)
+    private CodeLocation codeLocation;
+
+    @Column(name = "ABSOLUTE_PATH", nullable = false)
+    private String absolutePath;
+
+    @Column(name = "RELATIVE_PATH", nullable = false)
+    private String relativePath;
+
+    @Column(name = "IS_DIRECTORY", nullable = false)
+    private Boolean isDirectory = false;
+
+    @Column(name = "REVIEWED", nullable = false)
+    private Boolean reviewed = false;
+
+    @CreatedDate
+    @Column(name = "CREATED_DATE")
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_DATE")
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "LAST_MODIFIED_BY")
+    private String lastModifiedBy;
+
+    public File() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public File getParent() {
+        return parent;
+    }
+
+    public void setParent(File parent) {
+        this.parent = parent;
+    }
+
+    public CodeLocation getCodeLocation() {
+        return codeLocation;
+    }
+
+    public void setCodeLocation(CodeLocation codeLocation) {
+        this.codeLocation = codeLocation;
+    }
+
+    public String getAbsolutePath() {
+        return absolutePath;
+    }
+
+    public void setAbsolutePath(String absolutePath) {
+        this.absolutePath = absolutePath;
+    }
+
+    public String getRelativePath() {
+        return relativePath;
+    }
+
+    public void setRelativePath(String relativePath) {
+        this.relativePath = relativePath;
+    }
+
+    public Boolean getIsDirectory() {
+        return isDirectory;
+    }
+
+    public void setIsDirectory(Boolean directory) {
+        isDirectory = directory;
+    }
+
+    public Boolean getReviewed() {
+        return reviewed;
+    }
+
+    public void setReviewed(Boolean reviewed) {
+        this.reviewed = reviewed;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        File file = (File) o;
+        return Objects.equals(id, file.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "id=" + id +
+                ", fileName='" + fileName + '\'' +
+                '}';
+    }
+}
