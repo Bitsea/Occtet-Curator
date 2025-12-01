9deb3a7d1572e446f61631e89e7cd01e2a4b6c90
Patrick.Groeger <Patrick.Groeger@bitsea.de>
2025-12-01 10:09:45 +0100

    frontend: made export SBOM button functional

--- occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/project/ProjectListView.java
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/project/ProjectListView.java
@@ -19,19 +19,29 @@
 
 package eu.occtet.bocfrontend.view.project;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
-import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
+import eu.occtet.boc.model.SpdxExportWorkData;
+import eu.occtet.boc.model.WorkTask;
+
 import eu.occtet.bocfrontend.entity.Project;
-import eu.occtet.bocfrontend.entity.Vulnerability;
+import eu.occtet.bocfrontend.service.NatsService;
 import eu.occtet.bocfrontend.view.main.MainView;
-import eu.occtet.bocfrontend.view.vulnerability.VulnerabilityDetailView;
 import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 
 @Route(value = "projects", layout = MainView.class)
@@ -41,17 +51,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 @DialogMode(width = "64em")
 public class ProjectListView extends StandardListView<Project> {
 
-    @Autowired private UiComponents uiComponents;
+    private static final Logger log = LogManager.getLogger(ProjectListView.class);
 
-    @Supply(to = "projectsDataGrid.exportBtn", subject = "renderer")
-    private Renderer<Project> projectsDataGridExportBtnRenderer() {
-        return new ComponentRenderer<>(() -> {
+    @ViewComponent
+    private DataGrid<Project> projectsDataGrid;
+
+    @Autowired
+    private UiComponents uiComponents;
+    @Autowired
+    private NatsService natsService;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        DataGrid.Column<Project> exportColumn = projectsDataGrid.getColumnByKey("exportBtn");
+
+        exportColumn.setRenderer(new ComponentRenderer<>(project -> {
+
             JmixButton exportButton = uiComponents.create(JmixButton.class);
             exportButton.setIcon(VaadinIcon.DOWNLOAD.create());
             exportButton.setText("Export SBOM");
             exportButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SMALL);
             exportButton.setTooltipText("Export Project");
+            exportButton.addClickListener(clickEvent -> {
+                handleExport(project);
+            });
+
             return exportButton;
-        });
+        }));
     }
+
+    private void handleExport(Project project){
+        //TODO replace project.getProjectName with the corresponding spdxDocumentID
+        SpdxExportWorkData spdxExportWorkData = new SpdxExportWorkData(project.getProjectName(), String.valueOf(project.getId()));
+        LocalDateTime now = LocalDateTime.now();
+        long actualTimestamp = now.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+        WorkTask workTask = new WorkTask("processing_spdx", "send project data to microservice to create new spdx", actualTimestamp, spdxExportWorkData);
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+            String message = mapper.writeValueAsString(workTask);
+            log.debug("sending message to export service: {}", message);
+            natsService.sendWorkMessageToStream("work.export", message.getBytes(Charset.defaultCharset()));
+        }catch(Exception e){
+            log.error("Error with microservice connection: {}", e.getMessage());
+        }
+    }
+
 }
