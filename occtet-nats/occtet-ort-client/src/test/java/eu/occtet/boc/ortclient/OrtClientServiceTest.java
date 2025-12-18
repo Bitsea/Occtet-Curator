fd10a641a21d4ac3aa9714031f1123d279c29876
upost <uwe.post@upcenter.de>
2025-12-18 17:26:48 +0100

    demo code for accessing the ort server

--- occtet-nats/occtet-ort-client/src/test/java/eu/occtet/boc/ortclient/OrtClientServiceTest.java
+++ occtet-nats/occtet-ort-client/src/test/java/eu/occtet/boc/ortclient/OrtClientServiceTest.java
@@ -12,6 +12,7 @@ import javax.net.ssl.X509ExtendedTrustManager;
 import java.net.Socket;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -40,42 +41,71 @@ public class OrtClientServiceTest extends TestCase {
     /**
      * test&demonstrate how to access ORT server via OrtClientService.
      * This is NOT a unit test and thus not annotated with @Test because it requires a running ORT server with reachable Keycloak instance
-     * with the given configuration (default local docker setup).
+     * with the given configuration.
      * @throws Exception
      */
     public void testOrtClientAccess() throws Exception {
 
-
+        // REMINDER: you can use the following lines for local testing against a local ORT server
         //OrtClientService ortClientService = new OrtClientService("http://localhost:8080");
-        OrtClientService ortClientService = new OrtClientService("https://dev.bitsea.de");
         //AuthService authService = new AuthService("http://localhost:8081/realms/master/protocol/openid-connect/token");
+
+        // get bearer token from Keycloak
         AuthService authService = new AuthService("https://keycloak.bitsea.de/realms/master/protocol/openid-connect/token");
         TokenResponse tokenResponse = authService.requestToken("ort-server","ort-admin","password","offline_access");
+        System.out.println("Obtained TokenResponse: " + tokenResponse);
+
+        // start working with ORT API
+        OrtClientService ortClientService = new OrtClientService("https://ort.bitsea.de");
         ApiClient apiClient = ortClientService.createApiClient(tokenResponse);
 
+        // how to access version api
         VersionsApi versionsApi = new VersionsApi(apiClient);
         Map<String, String> versions = versionsApi.getVersions();
         versions.keySet().forEach(k-> System.out.println("Version key: " + k + " value: " + versions.get(k)));
 
+        // how to access organizations api
         OrganizationsApi organizationsApi = new OrganizationsApi(apiClient);
         PagedResponseOrganization orgs = organizationsApi.getOrganizations(null,null,null,null);
-        orgs.getData().forEach(o-> System.out.println("Organization: " + o));
-        Long organizationId = orgs.getData().get(0).getId();
+        List<Organization> data = orgs.getData();
+        if(data.isEmpty()) {
+            // nothing there? create an example organization
+            PostOrganization po = new PostOrganization();
+            po.setName("My Organization");
+            po.setDescription("My Organization Description");
+            organizationsApi.postOrganization(po);
+        }
+        data.forEach(o-> System.out.println("Organization: " + o));
+        Long organizationId = data.get(0).getId();
 
+        // how to access products api
         PagedResponseProduct organizationProducts = organizationsApi.getOrganizationProducts(organizationId, null, null, null, null);
+        if(organizationProducts.getData().isEmpty()) {
+            // if nothing there, create
+            PostProduct pp = new PostProduct();
+            pp.setName("My Product");
+            pp.setDescription("My Product Description");
+            organizationsApi.postProduct(organizationId, pp);
+            organizationProducts = organizationsApi.getOrganizationProducts(organizationId, null, null, null, null);
+        }
+
+        // now we have products
         organizationProducts.getData().forEach(p-> System.out.println("Organization Product: " + p));
         Long productId = organizationProducts.getData().get(0).getId();
 
+        // get product repositories
         ProductsApi productsApi = new ProductsApi(apiClient);
         PagedResponseRepository productRepositories = productsApi.getProductRepositories(productId, null, null, null, null);
         productRepositories.getData().forEach(r-> System.out.println("Product Repository: " + r));
         Long repositoryId = productRepositories.getData().get(0).getId();
 
+        // get repository runs
         RepositoriesApi repositoriesApi = new RepositoriesApi(apiClient);
         PagedResponseOrtRunSummary repositoryRuns = repositoriesApi.getRepositoryRuns(repositoryId, null, null, null);
         repositoryRuns.getData().forEach(r-> System.out.println("Repository Run: " + r));
         Long runId = repositoryRuns.getData().get(0).getId();
 
+        // get run statistics
         RunsApi runsApi = new RunsApi(apiClient);
         OrtRunStatistics runStatistics = runsApi.getRunStatistics(runId);
         System.out.println("Run Statistics: " + runStatistics);
