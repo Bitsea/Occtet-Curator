df8c5bcad56754823d6f73eb8cc3f27fdaf8e019
Ibrahim.AlBahra <ibrahim.albahra@bitsea.de>
2026-02-12 12:26:35 +0100

    feat: introduced `ExternalNotesConstants` and `FileConstants` utility classes with predefined constants for improved reusability and standardization

---
+++ occtet-nats/occtet-common/src/main/java/eu/occtet/boc/util/ExternalNotesConstants.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (C) 2025 Bitsea GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https:www.apache.orglicensesLICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  License-Filename: LICENSE
+ *
+ *
+ */
+
+package eu.occtet.boc.util;
+
+public class ExternalNotesConstants {
+
+    public static final String SECTION_SEPARATOR = "\n---\n";
+
+    // general
+    public static final String WARNING_AUDITOR_ATTENTION_REQ = "WARNING - Auditor's Attention Required:\n";
+
+    // Download Service
+    public static final String DOWNLOAD_SERVICE_FAILURE_MSG = "Failed to download resources. Please try downloading " +
+            "the files for this software component manually.";
+}
