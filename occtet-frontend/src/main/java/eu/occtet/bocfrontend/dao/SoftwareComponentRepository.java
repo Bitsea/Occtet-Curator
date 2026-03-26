a347222441e90d894c97a550d12ab2cd05bd3789
Lelalue <leoni.tischer@bitsea.de>
2026-03-26 12:19:48 +0100

    add component view

--- occtet-frontend/src/main/java/eu/occtet/bocfrontend/dao/SoftwareComponentRepository.java
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/dao/SoftwareComponentRepository.java
@@ -20,6 +20,7 @@
 package eu.occtet.bocfrontend.dao;
 
 
+import eu.occtet.bocfrontend.entity.License;
 import eu.occtet.bocfrontend.entity.Project;
 import eu.occtet.bocfrontend.entity.SoftwareComponent;
 import eu.occtet.bocfrontend.entity.Vulnerability;
@@ -40,6 +41,7 @@ public interface SoftwareComponentRepository extends JmixDataRepository<Software
     List<SoftwareComponent> findByVulnerability(Vulnerability vuln);
     @Query("select distinct sc from InventoryItem i join i.project p join i.softwareComponent sc where p = :project")
     List<SoftwareComponent> findByProject(Project project);
+
 
 
 }
---
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/dialog/AddSoftwareComponentDialog.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2025 Bitsea GmbH
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       https:www.apache.orglicensesLICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *   SPDX-License-Identifier: Apache-2.0
+ *   License-Filename: LICENSE
+ */
+
+package eu.occtet.bocfrontend.view.dialog;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.textfield.TextField;
+import eu.occtet.bocfrontend.dao.SoftwareComponentRepository;
+import eu.occtet.bocfrontend.entity.License;
+import eu.occtet.bocfrontend.entity.SoftwareComponent;
+import eu.occtet.bocfrontend.entity.Vulnerability;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.model.CollectionContainer;
+import io.jmix.flowui.view.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@ViewController("addSoftwareComponentDialog")
+@ViewDescriptor("add-software-component-dialog.xml")
+@DialogMode(width = "70%", height = "70%")
+public class AddSoftwareComponentDialog extends AbstractAddContentDialog<Vulnerability>{
+
+
+    private static final Logger log = LogManager.getLogger(AddSoftwareComponentDialog.class);
+
+    private Vulnerability vulnerability;
+
+    @Autowired
+    private SoftwareComponentRepository softwareComponentRepository;
+
+    @ViewComponent
+    private CollectionContainer<SoftwareComponent> softwareComponentDc;
+
+    @ViewComponent
+    private TextField searchField;
+
+    @ViewComponent
+    private DataGrid<SoftwareComponent> softwareComponentDataGrid;
+
+    @Override
+    @Subscribe("softwareComponentDc")
+    public void setAvailableContent(Vulnerability vulnerability){
+        this.vulnerability = vulnerability;
+        log.debug("Dialog context initialized with SoftwareComponent: {}", this.vulnerability);
+        softwareComponentDc.setItems(softwareComponentRepository.findAll());
+    }
+
+    @Override
+    @Subscribe(id = "addComponentButton")
+    public void addContentButton(ClickEvent<Button> event) {
+        List<SoftwareComponent> selectedLicenses = getSelectedComponents();
+
+        if (!selectedLicenses.isEmpty() && vulnerability != null) {
+            close(StandardOutcome.SAVE);
+        }
+    }
+
+    @Override
+    @Subscribe(id = "searchButton")
+    public void searchContentButton(ClickEvent<Button> event) {
+        String searchWord = searchField.getValue();
+
+        if (!searchWord.isEmpty() && event != null) {
+            List<SoftwareComponent> listFindings = softwareComponentRepository.findAll().stream()
+                    .filter(s -> s.getName().toLowerCase().contains(searchWord.toLowerCase())
+                            || s.getPurl().toLowerCase().contains(searchWord.toLowerCase()))
+                    .toList();
+            softwareComponentDc.setItems(listFindings);
+        } else {
+            softwareComponentDc.setItems(softwareComponentRepository.findAll());
+        }
+    }
+
+    @Subscribe(id = "cancelButton")
+    public void cancelLicense(ClickEvent<Button> event){
+        cancelButton(event);
+    }
+
+    public List<SoftwareComponent> getSelectedComponents() {
+        return new ArrayList<>(softwareComponentDataGrid.getSelectedItems());
+    }
+}
