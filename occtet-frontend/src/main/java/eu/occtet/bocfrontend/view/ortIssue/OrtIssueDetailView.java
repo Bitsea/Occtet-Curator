c18718d1de587a1e6fd87ff0937b20f81f6e981b
Alessandro.Castielli <Alessandro.Castielli@bitsea.de>
2026-04-07 16:16:53 +0200

    Fixed bug in ortIssue and ortViolation

--- occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/ortIssue/OrtIssueDetailView.java
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/ortIssue/OrtIssueDetailView.java
@@ -1,17 +1,51 @@
 package eu.occtet.bocfrontend.view.ortIssue;
 
 
+import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.router.Route;
+import eu.occtet.bocfrontend.dao.InventoryItemRepository;
+import eu.occtet.bocfrontend.dao.ProjectRepository;
+import eu.occtet.bocfrontend.entity.InventoryItem;
 import eu.occtet.bocfrontend.entity.OrtIssue;
+import eu.occtet.bocfrontend.entity.Project;
 import eu.occtet.bocfrontend.view.main.MainView;
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.component.combobox.JmixComboBox;
+import io.jmix.flowui.view.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "ortIssue/:id", layout = MainView.class)
 @ViewController(id = "OrtIssue.detail")
 @ViewDescriptor(path = "ort-issue-view.xml")
 @EditedEntityContainer("ortIssueDc")
 public class OrtIssueDetailView extends StandardDetailView<OrtIssue> {
+
+    private static final Logger log = LogManager.getLogger(OrtIssueDetailView.class);
+
+    @ViewComponent
+    private JmixComboBox<Project> projectField;
+    @ViewComponent
+    private JmixComboBox<InventoryItem> inventoryItemField;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    private InventoryItemRepository inventoryItemRepository;
+
+
+    @Subscribe
+    public void onInit(InitEvent event){
+        projectField.setItems(projectRepository.findAll());
+        projectField.setItemLabelGenerator(p -> p.getProjectName() + " - " +p.getVersion());
+    }
+
+    @Subscribe(id = "projectField")
+    public void chooseProject(final AbstractField.ComponentValueChangeEvent<JmixComboBox<Project>, Project> event){
+        Project project = event.getValue();
+        if(project != null){
+            inventoryItemField.setItems(inventoryItemRepository.findByProject(project));
+            inventoryItemField.setItemLabelGenerator(InventoryItem::getInventoryName);
+        }
+    }
 }
--- occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/ortViolation/OrtViolationDetailView.java
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/ortViolation/OrtViolationDetailView.java
@@ -1,13 +1,19 @@
 package eu.occtet.bocfrontend.view.ortViolation;
 
 
+import com.vaadin.flow.component.AbstractField;
+
 import com.vaadin.flow.router.Route;
+import eu.occtet.bocfrontend.dao.InventoryItemRepository;
+import eu.occtet.bocfrontend.dao.ProjectRepository;
+import eu.occtet.bocfrontend.entity.InventoryItem;
 import eu.occtet.bocfrontend.entity.OrtViolation;
+import eu.occtet.bocfrontend.entity.Project;
 import eu.occtet.bocfrontend.view.main.MainView;
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.component.combobox.JmixComboBox;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Route(value = "ortViolation/:id", layout = MainView.class)
 @ViewController(id = "OrtViolation.detail")
@@ -15,5 +21,29 @@ import io.jmix.flowui.view.ViewDescriptor;
 @EditedEntityContainer("ortViolationDc")
 public class OrtViolationDetailView extends StandardDetailView<OrtViolation> {
 
+    @ViewComponent
+    private JmixComboBox<Project> projectField;
+    @ViewComponent
+    private JmixComboBox<InventoryItem> inventoryItemField;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    private InventoryItemRepository inventoryItemRepository;
+
+
+    @Subscribe
+    public void onInit(InitEvent event){
+        projectField.setItems(projectRepository.findAll());
+        projectField.setItemLabelGenerator(p -> p.getProjectName() + " - " +p.getVersion());
+    }
+
+    @Subscribe(id = "projectField")
+    public void chooseProject(final AbstractField.ComponentValueChangeEvent<JmixComboBox<Project>, Project> event){
+        Project project = event.getValue();
+        if(project != null){
+            inventoryItemField.setItems(inventoryItemRepository.findByProject(project));
+            inventoryItemField.setItemLabelGenerator(InventoryItem::getInventoryName);
+        }
+    }
 }
