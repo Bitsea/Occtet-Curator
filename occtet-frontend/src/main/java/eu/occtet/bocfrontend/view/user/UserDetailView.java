484ab4a2a3cec950590d77e45a8d5809abeb7da6
Lelalue <leoni.tischer@bitsea.de>
2026-09-17 09:42:07 +0200

    fixing error in user view, handling keycloak

--- occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/user/UserDetailView.java
+++ occtet-frontend/src/main/java/eu/occtet/bocfrontend/view/user/UserDetailView.java
@@ -27,17 +27,23 @@ import eu.occtet.bocfrontend.dao.OrganizationRepository;
 import eu.occtet.bocfrontend.entity.Organization;
 import eu.occtet.bocfrontend.entity.User;
 import eu.occtet.bocfrontend.view.main.MainView;
+import io.jmix.core.DataManager;
 import io.jmix.core.EntityStates;
 import io.jmix.flowui.Notifications;
 import io.jmix.flowui.component.combobox.JmixComboBox;
 import io.jmix.flowui.component.textfield.TypedTextField;
 import io.jmix.flowui.view.*;
+import io.jmix.security.model.ResourceRole;
+import io.jmix.security.role.ResourceRoleRepository;
+import io.jmix.security.role.assignment.RoleAssignment;
+import io.jmix.security.role.assignment.RoleAssignmentRepository;
+import io.jmix.securitydata.entity.RoleAssignmentEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.TimeZone;
+import java.util.*;
 
 @Route(value = "users/:id", layout = MainView.class)
 @ViewController(id = "User.detail")
@@ -47,6 +53,11 @@ public class UserDetailView extends StandardDetailView<User> {
 
     @ViewComponent
     private TypedTextField<String> usernameField;
+
+    @Autowired
+    private ResourceRoleRepository resourceRoleRepository;
+    @Autowired
+    private RoleAssignmentRepository roleAssignmentRepository;
     @ViewComponent
     private PasswordField passwordField;
     @ViewComponent
@@ -57,22 +68,33 @@ public class UserDetailView extends StandardDetailView<User> {
     private MessageBundle messageBundle;
     @Autowired
     private Notifications notifications;
-
+    @ViewComponent
+    private JmixComboBox<ResourceRole> roleField;
     @Autowired
     private EntityStates entityStates;
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private DataManager dataManager;
     @Autowired
+    private PasswordEncoder passwordEncoder;
+    @ViewComponent
     private JmixComboBox<Organization> organization;
     @Autowired
     private OrganizationRepository organizationRepository;
+    @Autowired
+    private Environment environment;
+
+
+    private boolean isOidcActive() {
+        return environment.acceptsProfiles(Profiles.of("oidc"));
+    }
 
     @Subscribe
     public void onInit(final InitEvent event) {
+        List<ResourceRole> roles = new ArrayList<>(resourceRoleRepository.getAllRoles());
+        roleField.setItems(roles);
+        roleField.setItemLabelGenerator(ResourceRole::getName);
+
         organization.setItems(organizationRepository.findAll());
-        if(this.getEditedEntity().getOrganization()!= null){
-            organization.setValue(this.getEditedEntity().getOrganization());
-        }
         timeZoneField.setItems(List.of(TimeZone.getAvailableIDs()));
     }
 
@@ -81,6 +103,27 @@ public class UserDetailView extends StandardDetailView<User> {
         usernameField.setReadOnly(false);
         passwordField.setVisible(true);
         confirmPasswordField.setVisible(true);
+    }
+
+    @Subscribe
+    public void onBeforeShow(final BeforeShowEvent event) {
+        if (isOidcActive()) {
+            // deactivate in oidc modus, keycloak is master here
+            roleField.setReadOnly(true);
+            roleField.setHelperText("Rollen werden extern über Keycloak verwaltet.");
+        }
+        if (!entityStates.isNew(getEditedEntity())) {
+            Collection<RoleAssignment> assignments = roleAssignmentRepository
+                    .getAssignmentsByUsername(getEditedEntity().getUsername());
+
+            assignments.stream()
+                    .filter(a -> "resource".equals(a.getRoleType()))
+                    .findFirst()
+                    .ifPresent(assignment -> {
+                        ResourceRole role = resourceRoleRepository.findRoleByCode(assignment.getRoleCode());
+                        roleField.setValue(role);
+                    });
+        }
     }
 
     @Subscribe
@@ -107,6 +150,33 @@ public class UserDetailView extends StandardDetailView<User> {
                     .withType(Notifications.Type.WARNING)
                     .withPosition(Notification.Position.TOP_END)
                     .show();
+        }
+
+        //give user role
+        if (!isOidcActive()) {
+        ResourceRole selectedRole = roleField.getValue();
+        if (selectedRole != null) {
+            String username = getEditedEntity().getUsername();
+
+            // remove existing role
+            List<RoleAssignmentEntity> existingAssignments = dataManager.load(RoleAssignmentEntity.class)
+                    .query("select e from sec_RoleAssignmentEntity e where e.username = :username and e.roleType = :roleType")
+                    .parameter("username", username)
+                    .parameter("roleType", "resource")
+                    .list();
+
+            if (!existingAssignments.isEmpty()) {
+                dataManager.remove(existingAssignments);
+            }
+
+            // save new role
+            RoleAssignment newAssignment = new RoleAssignment(
+                    username,
+                    selectedRole.getCode(),
+                    "resource"
+            );
+            dataManager.save(newAssignment);
+        }
         }
     }
 }
